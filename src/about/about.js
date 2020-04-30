c616923f1ad21b9a22b48ea47c34e0f820d81e39
Филатов Алексей Алексеевич <Filatov.A.Alekseev@sberbank.ru>
2020-04-30 22:50:20 +0300

    start to working on about page

--- src/about/about.js
+++ src/about/about.js
@@ -1,3 +1,44 @@
 import './about.css';
+import Swiper from 'swiper';
 
-console.log("Hello, Webpack!")
+const prevButton = document.querySelector('.swiper-button-prev');
+const nextButton = document.querySelector('.swiper-button-next');
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    simulateTouch: false,
+    centeredSlides: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    /* on: {
+        slideChange() {
+            if (this.activeIndex === 1) {
+                prevButton.style.display = 'none';
+            }
+            else {
+                prevButton.style.display = 'flex';
+            }
+
+            if (this.activeIndex == this.slides.length - 2) {
+                nextButton.style.display = 'none';
+            }
+
+            else {
+                nextButton.style.display = 'flex';
+            }
+        }
+    } */
+});
+
+swiper.slideTo(3)
+
+
